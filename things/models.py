49240c79a070382a1fbba6e7690e0585ea20ece7
Yara Niroukh <yaraniroukh@hotmail.com>
2023-10-17 17:52:04 +0100

    Thing model: bug fixes)

--- things/models.py
+++ things/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.db.models import Model
 from django.core.validators import MinValueValidator
 from django.core.validators import MaxValueValidator
-from django.contrib.auth.models import AbstractUser
 
-class Thing(AbstractUser):
+class Thing(Model):
     name = models.CharField(unique=True, blank=False, max_length=30)
     description = models.CharField(unique=False, blank=True, max_length=120)
     quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0, "Must be at least 0"), MaxValueValidator(100, "Must be at most 100")], )
