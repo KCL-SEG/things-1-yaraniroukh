3149d22e7575ee2efa619b53ebc3fb728e8adbf3
Yara Niroukh <yaraniroukh@hotmail.com>
2023-10-17 17:48:27 +0100

    Add Thing model with validation

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Thing(AbstractUser):
+    name = models.CharField(unique=True, blank=False, max_length=30)
+    description = models.CharField(unique=False, blank=True, max_length=120)
+    quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0, "Must be at least 0"), MaxValueValidator(100, "Must be at most 100")], )
+
